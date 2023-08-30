9e39037d419805f53cc927b9f3d74e25a25c3073
dmizinchenk <85954157+dmizinchenk@users.noreply.github.com>
2023-08-30 22:04:56 +0300

    Update functions.php
    function style done

--- pages/functions.php
+++ pages/functions.php
@@ -18,19 +18,14 @@
         $pass=trim(htmlspecialchars($pass));
         $email=trim(htmlspecialchars($email));
         if ($name=="" || $pass=="" || $email=="") {
-            echo "<h3/><span style='color:red;'>
-            Fill All Required Fields!
-            </span><h3/>";
+            echo "<h3/><span style='color:red;'>Fill All Required Fields!</span><h3/>";
             return false;
         }
         if (strlen($name)<3 || strlen($name)>30 ||
             strlen($pass)<3 ||
             strlen($pass)>30) 
         {
-            echo "<h3/><span style='color:red;'>
-            Values Length Must Be Between 3
-            And 30!
-            </span><h3/>";
+            echo "<h3/><span style='color:red;'>Values Length Must Be Between 3 And 30!</span><h3/>";
             return false;
         }
         $ins='insert into users (login,pass,email,roleid) values("'.$name.'","'. md5($pass).'","'.$email.'",2)';
@@ -39,13 +34,9 @@
         $err=mysqli_errno($msq);
         if ($err){
             if($err==1062)
-                echo "<h3/><span style='color:red;'>
-                This Login Is Already Taken!
-                </span><h3/>";
+                echo "<h3/><span style='color:red;'>This Login Is Already Taken!</span><h3/>";
             else
-                echo "<h3/><span style='color:red;'>
-                Error code:".$err."!</
-                span><h3/>";
+                echo "<h3/><span style='color:red;'>Error code:".$err."!</span><h3/>";
          return false;
         }
         return true;
